cccbf1c621a430e15a7b1a2f3605a9e2b86ee6dc
jp <jordanpflem@gmail.com>
2021-07-31 11:57:12 -0700

    Modularized pointer.js

--- src/World/components/pointer.js
+++ src/World/components/pointer.js
@@ -2,66 +2,93 @@ import * as THREE from '../../../vendor/three/build/three.module.js';
 
 function createPointer(scene, rayCaster, camera, plane, objects, ghostBlock, createBlock, selected, isKeyDown, fn) {
     const pointer = new THREE.Vector2();
-
     var blockLength = ghostBlock.geometry.parameters.width;
 
+    listenForPointerMove(pointer, rayCaster, camera, objects, ghostBlock, blockLength, fn);
+    listenForPointerDown(pointer, scene, rayCaster, camera, plane, objects, createBlock, selected, isKeyDown, blockLength, fn);
+
+    return pointer;
+}
+
+function listenForPointerMove (pointer, rayCaster, camera, objects, ghostBlock, blockLength, fn) {
     document.addEventListener('pointermove', function (event) {
-        pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+        setPointerPosition(pointer, event);
         rayCaster.setFromCamera(pointer, camera);
 
-        const intersects = rayCaster.intersectObjects(objects);
-        if (intersects.length > 0) {
-            const intersect = intersects[0];
-
-            ghostBlock.position.copy(intersect.point).add(intersect.face.normal);
-            ghostBlock.position.divideScalar(blockLength).floor().multiplyScalar(blockLength).addScalar(blockLength / 2);
+        var intersect = getIntersectingObject(rayCaster, objects);
+        if (intersect) {
+            setBlockPosition(ghostBlock, blockLength, intersect);
         }
 
         fn();
     });
+}
 
+function listenForPointerDown (pointer, scene, rayCaster, camera, plane, objects, createBlock, selected, isKeyDown, blockLength, fn) {
     document.addEventListener('pointerdown', function (event) {
-        pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+        setPointerPosition(pointer, event);
         rayCaster.setFromCamera( pointer, camera );
 
-        const intersects = rayCaster.intersectObjects( objects );
-            if ( intersects.length > 0 ) {
-                 const intersect = intersects[ 0 ];
-                 selected[0] = intersect.object;
+        var intersect = getIntersectingObject(rayCaster, objects);
+        if (intersect) {
+            selected[0] = intersect.object;
 
-                // delete block
-                if ( isKeyDown['shift'] ) {
-                    if ( intersect.object !== plane ) {
-                        scene.remove( intersect.object );
-                        objects.splice( objects.indexOf( intersect.object ), 1 );
-                    }
-
+            if (isKeyDown['shift']) {
+                deleteBlock(scene, intersect, objects, plane);
+            } else {
+                if (blockIsSelected(intersect, plane)) {
+                    selectedBlockHandler(selected);
                 } else {
-                    // if selecting existing block
-                    if ((intersect.object instanceof THREE.Mesh) && 
-                        (intersect.object !== plane)) {
-                        selected[0].material.color.setHex(0x444444);
-                    }
-                    else {
-                    // creating block
-                    const newBlock = createBlock(blockLength);
-                    newBlock.position.copy( intersect.point ).add( intersect.face.normal );
-                    newBlock.position.divideScalar(blockLength).floor().multiplyScalar(blockLength).addScalar(blockLength / 2);
-                    // voxel.position.y = 75;
-                    scene.add( newBlock );
-
-                    objects.push( newBlock );
-                    }   
-
+                    drawBlock(scene, createBlock, blockLength, intersect, objects);
                 }
-
-                fn();
-
             }
-
+            fn();
+        }
     });
+}
 
-    return pointer;
+function setPointerPosition(pointer, event) {
+    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+}
+
+function setBlockPosition(block, blockLength, intersect) {
+    block.position.copy(intersect.point).add(intersect.face.normal);
+    block.position.divideScalar(blockLength).floor().multiplyScalar(blockLength).addScalar(blockLength / 2);
+}
+
+function getIntersectingObject(rayCaster, objects) {
+    var intersect = "";
+    const intersects = rayCaster.intersectObjects(objects);
+
+    if (intersects.length > 0) {
+        intersect = intersects[0];
+    }
+
+    return intersect;
+}
+
+function deleteBlock(scene, intersect, objects, plane) {
+    if ( intersect.object !== plane ) {
+        scene.remove( intersect.object );
+        objects.splice( objects.indexOf( intersect.object ), 1 );
+    }
+}
+
+function blockIsSelected(intersect, plane) {
+    return (intersect.object instanceof THREE.Mesh) && 
+            (intersect.object !== plane);
+}
+
+function selectedBlockHandler(selected) {
+    selected[0].material.color.setHex(0x444444);
+}
+
+function drawBlock(scene, createBlock, blockLength, intersect, objects) {
+    const newBlock = createBlock(blockLength);
+    setBlockPosition(newBlock, blockLength, intersect);
+
+    scene.add(newBlock);
+    objects.push(newBlock);
 }
 
 export { createPointer };
